75154da392657062af41e2b4e115ee9aacd8f0e6
kaydabs <37093536+kaydabs@users.noreply.github.com>
2018-11-28 19:42:37 -0600

    Add files via upload

---
+++ Controllers/linksController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using appp.Models;
+
+namespace appp.Controllers
+{
+    public class linksController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: links
+        public ActionResult Index()
+        {
+            return View(db.links.ToList());
+        }
+
+        // GET: links/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            link link = db.links.Find(id);
+            if (link == null)
+            {
+                return HttpNotFound();
+            }
+            return View(link);
+        }
+
+        // GET: links/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: links/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "key,webLink,linkRole")] link link)
+        {
+            if (ModelState.IsValid)
+            {
+                db.links.Add(link);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(link);
+        }
+
+        // GET: links/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            link link = db.links.Find(id);
+            if (link == null)
+            {
+                return HttpNotFound();
+            }
+            return View(link);
+        }
+
+        // POST: links/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "key,webLink,linkRole")] link link)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(link).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(link);
+        }
+
+        // GET: links/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            link link = db.links.Find(id);
+            if (link == null)
+            {
+                return HttpNotFound();
+            }
+            return View(link);
+        }
+
+        // POST: links/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            link link = db.links.Find(id);
+            db.links.Remove(link);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/link.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace appp.Models
+{
+    public class link
+    {
+        [Key]
+        public int key { get; set; }
+        public string webLink { get; set; }
+        public string linkRole { get; set; }
+    }
+}
